036ad98895af49a70337f701e62932d68bfb422b
1ntere <kwonhc9337java@gmail.com>
2024-07-12 16:26:03 +0900

    0712 axios 수정은 했으나 아직 오류

--- tutorial-one/src/component/R26_Axios_Ex2.js
+++ tutorial-one/src/component/R26_Axios_Ex2.js
@@ -1,13 +1,14 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 
 const Axios_Ex2 = () => {
-    const [data, setData] = useState(null);
+    const [comments, setComments] = useState(null);
 
     useEffect (() => {
         axios.get("https://jsonplaceholder.typicode.com/comments")
+        //axios.get("localhost:8080/마이페이지/회원정보들")
             .then (res => {
-                setData(res.data);
+                setComments(res.comments);
             })
             .catch (() => {
                 alert("데이터를 가져오는데 실패했습니다.");
@@ -19,12 +20,13 @@ const Axios_Ex2 = () => {
             <h1>버튼을 클릭하면 JSON 안에 작성된 Comments 내용 가져오기</h1>
             <br/>
             <ul>
-                {comments.map(comment => (
-                    <li>
-                        <strong>USERID : </strong>{} <br/>
-                        <strong>ID : </strong>{} <br/>
-                        <strong>TITLE : </strong>{} <br/>
-                        <strong>Completed : </strong>{   ? 'Yes' : 'No'} <br/>
+                {/* {회원정보들.map (각각의정보들 => ())} */}
+                {comments.map (comment => (
+                    <li key={comment.id}>
+                        <strong>UserId : </strong>{comment.id} <br/>
+                        <strong>Name : </strong>{comment.name} <br/>
+                        <strong>Email : </strong>{comment.email} <br/>
+                        <strong>Body : </strong>{comment.body} <br/>
                     </li>
                 ))}
             </ul>
